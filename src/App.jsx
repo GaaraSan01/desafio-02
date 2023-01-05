566908918b74695a142458c9994c7ce859740236
Vinícius H <91447833+GaaraSan01@users.noreply.github.com>
2023-01-05 18:12:20 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { login } from './utils';
+import './index.css';
+import { useState } from 'react';
+
+// Instruções:
+// * Você tem um formulário de login INCOMPLETO
+// * Não é permitido adicionar novos elementos HTML
+// * Não é permitido usar refs
+//
+// Tarefas:
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo - Desabilite o botão de Login equanto você está executando o login.
+// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+
+export default function LoginForm() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+  const [isRequest, setIsRequest] = useState(false)
+  
+  const handleClick = () => {
+    const values = {
+      email: email,
+      password: password
+    }
+
+    setIsRequest(true)
+    setError(null)
+
+    login(values)
+    .then(() => {alert("Usuário logado com sucesso!!!")})
+    .catch((error) => setError(error.message))
+    .finally(() => {
+      setIsRequest(false)
+    })
+  } 
+  
+
+  return (
+    <div className='wrapper'>
+      <div className='login-form'>
+        <h1>Login Form 🐞</h1>
+        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+        {error && <div className='errorMessage'>{error}</div>}
+        <div className='row'>
+          <label htmlFor={'email'}>Email</label>
+          <input id={'email'} type={'email'} autoComplete='off' onChange={e => setEmail(e.target.value)} />
+        </div>
+        <div className='row'>
+          <label htmlFor={'password'}>Password</label>
+          <input id={'password'} type={'password'} onChange={e => setPassword(e.target.value)}/>
+        </div>
+
+        <div className='button'>
+          <button onClick={handleClick} disabled={email == '' || password.length < 6 || isRequest}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
